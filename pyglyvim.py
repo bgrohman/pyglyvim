9bc73dce9906f3fe6be4fa4d2bbc3f3399d51f01
Bryan Grohman <bgrohman@gmail.com>
2014-03-03 22:57:52 -0600

    Support for defining vim commands. Vim script to set up pyglyvim python stuff.
    Now your plugin's vim script can be as short as two lines.

--- pyglyvim.py
+++ pyglyvim.py
@@ -1,22 +1,41 @@
 # Pyglyvim
 # MIT License
 # See https://github.com/bgrohman/pyglyvim
-
+import sys
 import vim
 
 # Misc
 def cd(directory):
 	vim.command("cd {0}".format(directory))
 
+def get_current_directory():
+	return vim.eval("getcwd()")
+
+def get_variable(name):
+	return vim.eval(name)
+
+# Commands
+commands = {}
+def define_command(command_name, fn):
+	commands[command_name] = fn
+	vim.command("command! {0} python pyglyvim_call_command('{0}')".format(command_name))
+
+def call_command(command_name):
+	commands[command_name]()
+
 # Location list
 def clear_location_list():
 	vim.command("lexpr []")
 
 def add_line_to_location_list(line_number):
+	# TODO: Rewrite using setloclist
 	vim.command('laddexpr expand("%:.") . ":" . {0} . ":" . getline({0})'.format(line_number))
 
 def open_location_list():
 	vim.command("lwindow")
+
+def close_location_list():
+	vim.command("lclose")
 
 # Signs
 def clear_signs():
@@ -27,3 +46,6 @@ def define_sign(sign_name, text="*", text_highlight="Error"):
 
 def add_sign(sign_id, line_number, sign_name, file_path=vim.current.buffer.name):
 	vim.command('sign place {0} line={1} name={2} file={3}'.format(sign_id, line_number, sign_name, file_path))
+
+# handle vim commands defined via pyglyvim
+vim.command("python from pyglyvim import call_command as pyglyvim_call_command")
